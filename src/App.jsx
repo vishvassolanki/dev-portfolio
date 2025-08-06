9e66382267947b03c51fd5848dd3f04f2ba34fca
vishvassolanki <vishvassolanki08@gmail.com>
2025-08-06 19:39:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1 className="text-red-400">Hello Wolrd</h1>
+            <h2>This is Vishvas Solanki a passionate web developer</h2>
+        </>
+    );
+}
+
+export default App;
